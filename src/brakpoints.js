9592b5aed29260d133a0f7c3f661ecb30834854e
bonecahStefanie <stefaniesilvadeoliveira@yahoo.com.br>
2024-02-19 20:23:45 -0300

    atualizar

--- src/brakpoints.js
+++ src/brakpoints.js
@@ -1,13 +1,13 @@
 const size = {
     sm: '511px',
     md: '600px',
-    bg: '966',
+    bg: '966px',
 }
 
 const breakpoints = {
     sm: `(max-width: ${size.sm})`,
     md: `(max-width: ${size.md})`,
-    bg: `(max-width: ${size.bg})`
+    bg: `(max-width: ${size.bg})`,
 }
 
 export default breakpoints;
